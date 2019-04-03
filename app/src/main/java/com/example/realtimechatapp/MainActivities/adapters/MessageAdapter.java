e3fe4299685aca07df162ad249820cc704769b59
calderon123 <arnoldedict342@gmail.com>
2019-04-03 20:56:45 +0800

    yeahyea

---
+++ app/src/main/java/com/example/realtimechatapp/MainActivities/adapters/MessageAdapter.java
@@ -0,0 +1,94 @@
+package com.example.realtimechatapp.MainActivities.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.realtimechatapp.MainActivities.activities.Chat;
+import com.example.realtimechatapp.MainActivities.activities.Counselors;
+import com.example.realtimechatapp.MainActivities.activities.MessageActivity;
+import com.example.realtimechatapp.MainActivities.activities.UserMentor;
+import com.example.realtimechatapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.List;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder> {
+
+    public  static final int MSG_TYPE_LEFT = 0;
+    public  static final int MSG_TYPE_RIGHT= 1;
+    private UserMentor userMentor;
+    private Context mContext;
+    private List<Chat> mChat;
+    public String imageurl;
+
+    FirebaseUser firebaseUser;
+
+    public MessageAdapter(Context mContext, List<Chat> mChat, String imageurl){
+        this.mChat = mChat;
+        this.mContext = mContext;
+        this.imageurl = imageurl;
+    }
+
+    @NonNull
+    @Override
+    public MessageAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == MSG_TYPE_RIGHT) {
+            View view = LayoutInflater.from(mContext).inflate(R.layout.chat_item_right, parent, false);
+            return new MessageAdapter.ViewHolder(view);
+        }else{
+            View view = LayoutInflater.from(mContext).inflate(R.layout.chat_item_left, parent, false);
+            return new MessageAdapter.ViewHolder(view);
+        }
+    }
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        final Chat chat= mChat.get(i);
+
+
+        viewHolder.show_message.setText(chat.getMessage());
+
+
+            viewHolder.profile_image.setImageResource(R.mipmap.ic_launcher);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mChat.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView show_message;
+        public ImageView profile_image;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+
+            show_message = itemView.findViewById(R.id.show_message);
+            profile_image = itemView.findViewById(R.id.profile_image);
+
+        }
+    }
+
+
+    @Override
+    public int getItemViewType(int position) {
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (mChat.get(position).getSender().equals(firebaseUser.getUid())){
+            return MSG_TYPE_RIGHT;
+        }else{
+            return MSG_TYPE_LEFT;
+        }
+    }
+}
