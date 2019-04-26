869b4da2b82a7a8a6250f1e0a8b0e0f178f839eb
calderon123 <arnoldedict342@gmail.com>
2019-04-26 18:09:35 +0800

    yeah

--- app/src/main/java/com/example/realtimechatapp/MainActivities/Notification/MyFirebaseMessagin.java
+++ app/src/main/java/com/example/realtimechatapp/MainActivities/Notification/MyFirebaseMessagin.java
@@ -1,6 +1,7 @@
 package com.example.realtimechatapp.MainActivities.Notification;
 
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -8,7 +9,9 @@ import android.content.Intent;
 
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 
 import com.example.realtimechatapp.MainActivities.activities.MessageActivity;
@@ -17,6 +20,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+@SuppressWarnings("ALL")
 public class MyFirebaseMessagin extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
@@ -26,9 +30,44 @@ public class MyFirebaseMessagin extends FirebaseMessagingService {
 
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
+        assert sented != null;
         if (firebaseUser != null && sented.equals(firebaseUser.getUid())){
-            sendNotification(remoteMessage);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                sendOreoNotification (remoteMessage);
+            }else {
+                sendNotification(remoteMessage);
+            }
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private void sendOreoNotification(RemoteMessage remoteMessage) {
+        String user = remoteMessage.getData().get("user");
+        String icon = remoteMessage.getData().get("icon");
+        String title = remoteMessage.getData().get("title");
+        String body = remoteMessage.getData().get("body");
+
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+        assert user != null;
+        int j = Integer.parseInt(user.replaceAll("[\\D]",""));
+        Intent intent = new Intent(this, MessageActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("userid", user);
+        intent.putExtras(bundle);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, j ,intent ,PendingIntent.FLAG_ONE_SHOT);
+        Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        OreoNotification oreoNotification = new OreoNotification(this);
+        Notification.Builder builder  = oreoNotification.getOreoNotification(title, body ,
+                pendingIntent, defaultSound, icon);
+        int i = 0;
+        if (j > 0){
+            i =j;
+        }
+
+        oreoNotification.getManager().notify(i, builder.build());
     }
 
     private void sendNotification(RemoteMessage remoteMessage) {
@@ -39,6 +78,7 @@ public class MyFirebaseMessagin extends FirebaseMessagingService {
         String body = remoteMessage.getData().get("body");
 
         RemoteMessage.Notification notification = remoteMessage.getNotification();
+        assert user != null;
         int j = Integer.parseInt(user.replaceAll("[\\D]",""));
         Intent intent = new Intent(this, MessageActivity.class);
         Bundle bundle = new Bundle();
